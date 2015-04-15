9ecbf5b3e958f3f869ec3fa9522095a68a3ed3a2
evnhali <evnhali@gmail.com>
2015-04-15 05:44:21 -0700

    Refactored init.lua
    Removed non-embeddable Ace libraries from embeds list and registered
    them with other non-embeddable libraries. Moved non-embeddable library
    registrations from main addon namespace to utility namespace.

--- init.lua
+++ init.lua
@@ -6,26 +6,28 @@ local LibStub = LibStub
 
 -- Create addon with AceAddon
 local Metric  = LibStub('AceAddon-3.0'):NewAddon('Metric',
-                                                --mixins
+                                                --embeds
                                                 'AceBucket-3.0',
                                                 'AceComm-3.0',
-                                                'AceConfig-3.0',
                                                 'AceConsole-3.0',
-                                                'AceDB-3.0',
-                                                'AceDBOptions-3.0',
                                                 'AceEvent-3.0',
-                                                'AceGUI-3.0',
                                                 'AceHook-3.0',
-                                                'AceLocale-3.0',
                                                 'AceSerializer-3.0',
-                                                'AceTab-3.0',
                                                 'AceTimer-3.0')
 
--- Register non-mixin libraries
-Metric.CompressLib = LibStub:GetLibrary('LibCompress')
-Metric.SpellLib    = LibStub:GetLibrary('LibPlayerSpells-1.0')
-Metric.MediaLib    = LibStub:GetLibrary('LibSharedMedia-3.0')
+-- Create namespace for shared utility functions and libraries
+local Util    = {}
+
+-- Register non-embeddable libraries
+Util.CompressLib = LibStub('LibCompress')
+Util.SpellLib    = LibStub('LibPlayerSpells-1.0')
+Util.MediaLib    = LibStub('LibSharedMedia-3.0')
+Util.ConfigLib   = LibStub('AceConfig-3.0')
+Util.GUILib      = LibStub('AceGUI-3.0')
+Util.DBLib       = LibStub('AceDB-3.0')
+Util.DBOptLib    = LibStub('AceDBOptions-3.0')
+Util.LocaleLib   = LibStub('AceLocale-3.0')
 
 -- Share data with other Metric files
-ns.Util   = {}       -- namespace for utility functions
+ns.Util   = Util       
 ns.Metric = Metric
