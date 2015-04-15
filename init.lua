03f42237d78a307304bb6db52ca2ae19576b67b3
evnhali <evnhali@gmail.com>
2015-04-15 04:38:42 -0700

    Added init.lua
    Initialization script: create addon, register libraries

---
+++ init.lua
@@ -0,0 +1,27 @@
+-- Get local namespace
+local _, ns = ...
+
+-- Localize globals
+local LibStub = LibStub
+
+-- Register with AceAddon
+local Metric = LibStub('AceAddon-3.0'):NewAddon('Metric',
+                                                --mixins
+                                                'AceBucket-3.0',
+                                                'AceComm-3.0',
+                                                'AceConfig-3.0',
+                                                'AceConsole-3.0',
+                                                'AceDB-3.0',
+                                                'AceDBOptions-3.0',
+                                                'AceEvent-3.0',
+                                                'AceGUI-3.0',
+                                                'AceHook-3.0',
+                                                'AceLocale-3.0',
+                                                'AceSerializer-3.0',
+                                                'AceTab-3.0',
+                                                'AceTimer-3.0')
+ns.Metric = Metric
+
+Metric.CompressLib = LibStub:GetLibrary('LibCompress')
+Metric.SpellLib    = LibStub:GetLibrary('LibPlayerSpells-1.0')
+Metric.MediaLib    = LibStub:GetLibrary('LibSharedMedia-3.0')
