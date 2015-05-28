94a398129ca01e7ea01bd0a8ca7d189c83e02be2
evnhali <evnhali@gmail.com>
2015-05-28 01:49:29 -0700

    Started implementing encounters

--- modules/data/encounter.lua
+++ modules/data/encounter.lua
@@ -5,9 +5,24 @@ local _, ns = ...
 
 -- Localize variables
 local Metric, Private = ns.Metric, ns.Private
+local setmetatable = setmetatable
 
 local Data = Metric:GetModule('Data')
 local db = Data.db
+db.char.encounters = db.char.encounters or {}
+local encs = db.char.encounters
 
 local Encounter = Data:NewModule('Encounter')
 
+local curEnc
+
+function Encounter:New()
+	if curEnc then curEnc:Archive
+	curEnc = {}
+	setmetatable(curEnc, self)
+	return curEnc
+end
+
+function Encounter:Archive()
+	encs[#encs + 1] = Private.Lib.Compress.Compress(self)
+end
